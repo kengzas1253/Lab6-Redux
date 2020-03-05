5fe61c302237c2307a4fc58933670720d286f0ba
kengzas1253 <kengzas1253@gmail.com>
2020-03-05 13:23:04 +0700

    first commit

---
+++ redux-bear/src/App.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import BearCard from './components/BearCard'
+import BearList from './components/BearList'
+import InputForm from './components/InputForm';
+import { useSelector, useDispatch } from '../../../node_modules/react-redux';
+
+export default () => {
+
+  // const bears = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  //console.log(bears);
+
+  // const [bears, setBears] = useState({})
+  const [formData, setData] = useState({
+    name: '',
+    weight: 0,
+    img: ''
+  });
+
+  const { name, weight, img } = formData;
+
+  const getBears = async () => {
+    const result = await axios.get(`http://localhost/api/bears`)
+    // setBears(result.data)
+    //sent api to store code line 27-29
+    const action = {type:'GET_BEARS',bears: result.data}
+    dispatch(action)
+  }
+
+  const addBear = async () => {
+    const result = await axios.post(`http://localhost/api/bears/`, {
+      name,
+      weight,
+      img
+    })
+    console.log(result.data)
+    getBears()
+  }
+
+  const deleteBear = async (id) => {
+    const result = await axios.delete(`http://localhost/api/bears/${id}`)
+    console.log(result.data)
+    getBears()
+  }
+
+  const updateBear = async (id) => {
+    console.log(name, weight, img);
+    const result = await axios.put(`http://localhost/api/bears/${id}`, {
+      name,
+      weight,
+      img
+    })
+    console.log('bear id update: ', result.data)
+    getBears()
+  }
+
+  useEffect(() => {
+    getBears()
+  }, [])
+
+  return (
+    <div>
+      <h2>Bears</h2>
+      <BearList  deleteBear={deleteBear} updateBear={updateBear}/>
+      <InputForm/>
+    </div>
+  )
+}
---
+++ redux-bear/src/components/BearList.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react'
+import BearCard from './BearCard';
+import './BearList.css';
+import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios'
+
+const BearList = props => {
+
+    const bears = useSelector(state => state.bear);
+    const dispatch = useDispatch();
+    const getBears = async () => {
+        const result = await axios.get(`http://localhost/api/bears`)
+        // setBears(result.data)
+        //sent api to store code line 27-29
+        const action = {type:'GET_BEARS',bears: result.data}
+        dispatch(action)
+      }
+      useEffect(() => {
+        getBears()
+      }, [])
+
+
+
+   //check api
+    if (!bears || !bears.length)
+        return (<h2>No bears</h2>)
+
+    return (
+        <div className='bearlist-container'>
+            {
+                bears.map((bear, index) => (
+                    <div key={index} style={{ margin: 5 }}>
+                        <BearCard  {...bear}  />
+                    </div>
+                ))
+            }
+        </div>
+
+    )
+}
+
+export default BearList;
